4e0b4cb792ccc56954796932f225080b6e19b4a1
Elliana May <me@mause.me>
2022-07-13 12:59:27 +0000

    feat: try querying duckdb

--- api/index.ts
+++ api/index.ts
@@ -1,17 +1,28 @@
-import { Handler } from "@netlify/functions";
-import { readdir } from "fs/promises";
+import { Handler, HandlerResponse } from "@netlify/functions";
+import { Database } from "duckdb";
+import { query } from "../src/build_index";
+import { promisify } from "util";
+
+const json = (statusCode: number, body: any): HandlerResponse => ({
+  statusCode,
+  body: JSON.stringify(body, undefined, 2),
+});
 
 export const handler: Handler = async (event, ctx) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        hello: "world",
-        parentDir: await readdir(".."),
-        dir: await readdir("."),
-      },
-      undefined,
-      2
-    ),
-  };
+  const q = event.queryStringParameters?.q;
+  if (!q) {
+    return json(422, { error: "missing search query" });
+  }
+
+  const db = new Database("search_index.db");
+  const all = promisify(db.all.bind(db));
+
+  const results = all(query, q);
+
+  db.close();
+
+  return json(200, {
+    results,
+    query: q,
+  });
 };
