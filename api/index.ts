febe98ae23adb3f5b14b017c9bbd7d874fa1f2ee
Elliana May <me@mause.me>
2022-07-14 13:22:30 +0000

    chore: fix import

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import { query } from "../src/common";
 import Worker from "web-worker";
 import { dirname } from "path";
 import { AsyncDuckDB, ConsoleLogger, selectBundle } from "@duckdb/duckdb-wasm";
-import instantiateStreaming from 'wasm-instantiate-streaming';
+import { instantiateStreaming } from "wasm-instantiate-streaming";
 
 const base = dirname(require.resolve("@duckdb/duckdb-wasm")) + "/";
 const pair = (type: string) => ({
@@ -33,7 +33,11 @@ export const handler: Handler = async (event, ctx) => {
 
   const bundle = await selectBundle(DUCKDB_BUNDLES);
   WebAssembly.instantiateStreaming = instantiateStreaming;
-  console.log({ bundle, WebAssembly: WebAssembly.instantiateStreaming, XMLHttpRequest: global.XMLHttpRequest });
+  console.log({
+    bundle,
+    WebAssembly: WebAssembly.instantiateStreaming,
+    XMLHttpRequest: global.XMLHttpRequest,
+  });
 
   const db = new AsyncDuckDB(
     new ConsoleLogger(),
