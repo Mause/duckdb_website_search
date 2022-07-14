ef53ba714ff6a59d5fb948e081e5e2d3f3ebf9db
Elliana May <me@mause.me>
2022-07-14 11:56:18 +0000

    chore: missed file

---
+++ src/common.ts
@@ -0,0 +1,8 @@
+const queryBuilder = (index_schema: string, index_name: string) => `
+SELECT title, score
+FROM (SELECT *, fts_${index_schema}_${index_name}.match_bm25(title, ?) AS score
+    FROM ${index_name}) sq
+WHERE score IS NOT NULL
+ORDER BY score DESC;
+`;
+export const query = queryBuilder('main', 'search_index');
