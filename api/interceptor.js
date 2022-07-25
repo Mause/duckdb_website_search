5adddd8d44f6edda45b5ad271eee32db31e2df42
Elliana May <me@mause.me>
2022-07-25 03:38:00 +0000

    chore: add missing dirname import

--- api/interceptor.js
+++ api/interceptor.js
@@ -1,5 +1,6 @@
 require("whatwg-fetch");
 require('wasm-instantiate-streaming');
+const { dirname } = require("path");
 
 const url = dirname(require.resolve("@duckdb/duckdb-wasm")) + "/duckdb-browser-mvp.worker.js";
 
