47960567df1b2869e9e38faea47b762d364bdd3d
Elliana May <me@mause.me>
2022-07-25 03:41:49 +0000

    chore: add some logs

--- api/interceptor.js
+++ api/interceptor.js
@@ -4,4 +4,6 @@ const { dirname } = require("path");
 
 const url = dirname(require.resolve("@duckdb/duckdb-wasm")) + "/duckdb-browser-mvp.worker.js";
 
+console.log({url});
+console.log(WebAssembly.instantiateStreaming);
 require(url);
