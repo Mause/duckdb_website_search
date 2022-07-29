6944f890618e312c2ced30b28af998cbadbe8ef3
Elliana May <me@mause.me>
2022-07-29 14:55:44 +0800

    chore: try another method

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import { Handler } from "@netlify/functions";
 import { query } from "../src/common";
 import { json, initiate } from '../src/api_common';
-import { readFile } from 'fs/promises';
 
 export const handler: Handler = async (event, ctx) => {
   const q = event.queryStringParameters?.q;
@@ -12,8 +11,8 @@ export const handler: Handler = async (event, ctx) => {
   try {
     const db = await initiate();
     console.log('opening');
-    const path = __dirname + "/../search_index.db";
-    await db.registerFileHandle(path, await readFile(path));
+    const path = 'file:///' + __dirname + "/../search_index.db";
+    await db.registerFileURL(path, path);
     await db.open({ path });
     console.log('connecting');
     const conn = await db.connect();
