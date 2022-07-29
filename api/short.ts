9a6d8dd6d46317b2fe577dbb7c97dee5b1c43fc5
Elliana May <me@mause.me>
2022-07-29 04:47:36 +0000

    chore: move import

--- api/short.ts
+++ api/short.ts
@@ -1,12 +1,12 @@
 import { Handler } from "@netlify/functions";
 import { AsyncDuckDB } from "@duckdb/duckdb-wasm";
 import { json, initiate } from '../src/api_common';
-// @ts-ignore
-const bufferfromfile = await import('bufferfromfile');
 
 export const handler: Handler = async (_event, _ctx) => {
   let db: AsyncDuckDB;
   try {
+    // @ts-ignore
+    const bufferfromfile = await import('bufferfromfile');
     db = await initiate();
 
     const conn = await db.connect();
